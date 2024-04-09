ec1ed56ee4690836dca0b174536db3e827b63938
iiMu7aMMaD <muhammad.hadidi@outlook.com>
2024-04-09 21:03:15 +0200

    Made script to keep track of video time so it dont start over on every page change

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,23 @@ menuToggle.addEventListener("click", () => {
   menuToggle.classList.toggle("active");
   main.classList.toggle("active");
 });
+
+var video = document.getElementById("video");
+var storedTime = localStorage.getItem("videoTime");
+
+if (storedTime) {
+  video.currentTime = parseFloat(storedTime);
+}
+
+video.addEventListener("timeupdate", function () {
+  localStorage.setItem("videoTime", video.currentTime);
+});
+
+video.addEventListener(
+  "ended",
+  function () {
+    this.currentTime = 0;
+    this.play();
+  },
+  false
+);
